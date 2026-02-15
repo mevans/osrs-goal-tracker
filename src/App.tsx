bb23a803537207d609b1ffc160d335166fc3d579
Matthew Evans <matthew@evans99.co.uk>
2026-02-15 15:31:41 +0000

    Initial scaffold: Vite + React + TypeScript + Tailwind + Zustand + TanStack Query + React Router

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold p-8">OSRS Goal Tracker</h1>
+    </div>
+  )
+}
+
+export default App
